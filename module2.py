8793bab07ffe97604abbf046eb6a92574c9ae165
snoopy <140952627+Snoopy0411@users.noreply.github.com>
2023-09-09 16:17:22 +0530

    Updated module2.py

--- module2.py
+++ module2.py
@@ -91,7 +91,7 @@ def text():
     import cv2 as cv
     import numpy as np
     blank=np.zeros((500,500,3),dtype='uint8')
-    cv.putText(blank,'Tvisha Mohan',(0,255),cv.FONT_HERSHEY_TRIPLEX,1.0,(0,255,0),thickness=3)
+    cv.putText(blank,'Hello World',(0,255),cv.FONT_HERSHEY_TRIPLEX,1.0,(0,255,0),thickness=3)
     cv.imshow('Text',blank)
     cv.waitKey(0)
 
